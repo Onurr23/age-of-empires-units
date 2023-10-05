7923c4c3d9109e0ac895f2ad8fc430bced63157b
Onur Kara <60196841+Onurr23@users.noreply.github.com>
2023-10-05 21:41:49 +0300

    typography file is added

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import './sass/_global.scss';
+import './sass/_typography.scss';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
